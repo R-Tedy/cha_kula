166c0bca74e2c1fd1a1a4535a3be2f3b477e10a9
R-Tedy <stedyroland2@gmail.com>
2025-09-10 18:42:30 +0300

    set-up utility classes

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,23 @@
-import { FlatList, Text, View } from "react-native";
+import { offers } from "@/constants";
+import { FlatList, Pressable, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
     <SafeAreaView>
       <FlatList
-        
+        data={offers}
+        renderItem={({item, index}) => {
+          return (
+            <View>
+              <Pressable className="bg-amber-600">
+                <Text>
+                  {item.title}
+                </Text>
+              </Pressable>
+            </View>
+          )
+        }}
       />
     </SafeAreaView>
   );
