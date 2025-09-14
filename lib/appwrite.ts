98f349c8c5b6e5dbdd49e2a3b2efa2ae23c12e20
R-Tedy <stedyroland2@gmail.com>
2025-09-14 23:33:15 +0300

    setup appwrite

---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  platform: "com.ted.chakula",
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  databaseId: "68c724760000966352c7",
+  userTableId: "user",
+}
